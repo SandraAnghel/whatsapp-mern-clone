e50568e3257b3f7e11f5deb78472b3bb709b6c41
sandraanghel <sandra.anghel2701@gmail.com>
2022-05-20 20:20:24 +0100

    Implement frontend

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7lFZlhk41O7u2mQwtXvCwtGSndkMy9Kg",
+    authDomain: "whatsapp-mern-d0a6b.firebaseapp.com",
+    projectId: "whatsapp-mern-d0a6b",
+    storageBucket: "whatsapp-mern-d0a6b.appspot.com",
+    messagingSenderId: "1046937729029",
+    appId: "1:1046937729029:web:a1565e5fb33b441ab7c46c"
+  }; 
