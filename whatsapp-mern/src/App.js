60e4eed4258d4ba4092e1e5b680c943f6c1d3904
sandraanghel <sandra.anghel2701@gmail.com>
2022-05-21 15:10:06 +0100

    Real time connection to mongodb

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    const pusher =new Pusher("ce9f230e16f7181327b2", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className='app__body'>
